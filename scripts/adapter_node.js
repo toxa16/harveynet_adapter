12f4fa5c705948bce95e0896ac68a0d1b570586d
toxa16 <toxa16@gmail.com>
2020-06-03 17:51:10 +0300

    splitting into feature procedures

--- scripts/adapter_node.js
+++ scripts/adapter_node.js
@@ -44,78 +44,94 @@ const controlChannelName = `presence-control-${machineId}`;
 const controlChannel = pusher.subscribe(controlChannelName);
 
 
+// odometry & simulated GPS
+function odomProcedure(nh) {
+	const odomInterval = 500;
+	let odomTriggerAllowed = true;
+	setInterval(() => {
+		odomTriggerAllowed = true;
+	}, odomInterval);
+	nh.subscribe('/odom', 'nav_msgs/Odometry', msg => {
+		const { x, y } = msg.pose.pose.position;
+		console.log(`x: ${x}, y: ${y}`);
+		if (odomTriggerAllowed) {
+			odomTriggerAllowed = false;
+
+			// simulating GPS coords
+			const baseLat = 50.422;
+			const baseLng = 29.973;
+			const mLat = 5 / 10;
+			const mLng = 8 / 10;
+			const latitude = baseLat + y * 0.001 * mLat;
+			const longitude = baseLng + x * 0.001 * mLng;
+			channel.trigger('client-set-navsat', { latitude, longitude });
+			channel.trigger('client-set-coordinates', { x, y });
+		}
+	});
+}
+
+
+// camera
+function cameraProcedure(nh) {
+	const cameraTriggerInterval = 200;
+	let cameraTriggerAllowed = true;
+	setInterval(() => {
+		cameraTriggerAllowed = true;
+	}, cameraTriggerInterval);
+	nh.subscribe(
+		'/camera/rgb/image_raw/compressed',
+		'sensor_msgs/CompressedImage',
+		msg => {
+			if (cameraTriggerAllowed) {
+				const _image = Buffer.from(msg.data).toString('base64');
+				const image = 'data:image/jpg;base64,' + _image;
+				triggerChunked(channel, 'client-set-camera-image', { image });
+				cameraTriggerAllowed = false;
+			}
+		},
+	);
+}
+
+
+// movement control
+function moveControlProcedure(nh) {
+	const linearSpeed = 0.5;
+	const angularSpeed = 1;
+	const commandObj = {
+		lx: 0,
+		az: 0,
+	}
+	controlChannel.bind('client-move-command', command => {
+		const { l, a } = command;
+		commandObj.lx = l * linearSpeed;
+		commandObj.az = a * angularSpeed;
+	});
+	let controlTopic = '/cmd_vel';		// TurtleBot3
+	if (process.env.TURTLEBOT2) {
+		controlTopic = '/cmd_vel_mux/input/teleop';	// TurtleBot2
+	}
+	const pub = nh.advertise(controlTopic, 'geometry_msgs/Twist');
+	setInterval(() => {
+		const { lx, az } = commandObj;		
+		const msg = {
+			linear: { x: lx, y: 0, z: 0 },
+			angular: { x: 0, y: 0, z: az },
+		};
+		pub.publish(msg);
+	}, 10);
+}
+
+
 // init adapter node
 rosnodejs.initNode('/adapter')
-	.then(() => {		
-		const odomInterval = 500;
-		let odomTriggerAllowed = true;
-		setInterval(() => {
-			odomTriggerAllowed = true;
-		}, odomInterval);
+	.then(() => {
 		const nh = rosnodejs.nh;
-		// odometry
-		nh.subscribe('/odom', 'nav_msgs/Odometry', msg => {
-			const { x, y } = msg.pose.pose.position;
-			console.log(`x: ${x}, y: ${y}`);
-			if (odomTriggerAllowed) {
-				odomTriggerAllowed = false;
 
-				// simulating GPS coords
-				const baseLat = 50.422;
-    		const baseLng = 29.973;
-    		const mLat = 5 / 10;
-				const mLng = 8 / 10;
-				const latitude = baseLat + y * 0.001 * mLat;
-				const longitude = baseLng + x * 0.001 * mLng;
-				channel.trigger('client-set-navsat', { latitude, longitude });
-				channel.trigger('client-set-coordinates', { x, y });
-			}
-		});
+		// you can disable a feature by commenting out correspondind line below
 
-		// camera image
-		const cameraTriggerInterval = 200;
-		let cameraTriggerAllowed = true;
-		setInterval(() => {
-			cameraTriggerAllowed = true;
-		}, cameraTriggerInterval);
-		const sub2 = nh.subscribe(
-			'/camera/rgb/image_raw/compressed',
-			'sensor_msgs/CompressedImage',
-			msg => {
-				if (cameraTriggerAllowed) {
-					const _image = Buffer.from(msg.data).toString('base64');
-					const image = 'data:image/jpg;base64,' + _image;
-					triggerChunked(channel, 'client-set-camera-image', { image });
-					cameraTriggerAllowed = false;
-				}
-			},
-		);
-
-		// control
-		const linearSpeed = 0.5;
-		const angularSpeed = 1;
-		const commandObj = {
-			lx: 0,
-			az: 0,
-		}
-		controlChannel.bind('client-move-command', command => {
-			const { l, a } = command;
-			commandObj.lx = l * linearSpeed;
-			commandObj.az = a * angularSpeed;
-		});
-		let controlTopic = '/cmd_vel';		// TurtleBot3
-		if (process.env.TURTLEBOT2) {
-			controlTopic = '/cmd_vel_mux/input/teleop';	// TurtleBot2
-		}
-		const pub = nh.advertise(controlTopic, 'geometry_msgs/Twist');
-		setInterval(() => {
-			const { lx, az } = commandObj;		
-			const msg = {
-				linear: { x: lx, y: 0, z: 0 },
-				angular: { x: 0, y: 0, z: az },
-			};
-			pub.publish(msg);
-		}, 10);
+		odomProcedure(nh);
+		cameraProcedure(nh);
+		moveControlProcedure(nh);
 
 		// tool control
 		/*const bin1 = nh.advertise('/harvey/binary_1', 'std_msgs/Bool');
