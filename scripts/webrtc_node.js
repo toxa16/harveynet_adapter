a21a4c458735255f04345f7b0118c3e125f10e4e
toxa16 <toxa16@gmail.com>
2020-06-21 00:10:46 +0300

    init webrtc camera streaming

---
+++ scripts/webrtc_node.js
@@ -0,0 +1,142 @@
+#!/usr/bin/env node
+
+const rosnodejs = require('rosnodejs');
+const Pusher = require('pusher-js');
+const { RTCPeerConnection, RTCIceCandidate, RTCSessionDescription, MediaStream, nonstandard } = require('wrtc');
+const { RTCVideoSource, rgbaToI420 } = nonstandard;
+
+// utility
+function delay(ms) {
+  return new Promise(resolve => {
+    setTimeout(resolve, ms);
+  });
+}
+
+//Pusher.logToConsole = true;
+var pusher = new Pusher('fa20e14745781ba145ef', {
+  cluster: 'eu',
+  authEndpoint: 'https://harveynet-ownership-server.herokuapp.com/pusher/auth/machine',
+});
+const channelName = 'private-webrtc-sandbox';
+const offerEventName = 'client-offer';
+const answerEventName = 'client-answer';
+const iceEventName = 'client-ice';
+var channel = pusher.subscribe(channelName);
+
+
+const iceServers = [
+  /*{urls:'stun:stun01.sipphone.com'},
+  {urls:'stun:stun.ekiga.net'},
+  {urls:'stun:stun.fwdnet.net'},
+  {urls:'stun:stun.ideasip.com'},
+  {urls:'stun:stun.iptel.org'},
+  {urls:'stun:stun.rixtelecom.se'},
+  {urls:'stun:stun.schlund.de'},*/
+  {urls:'stun:stun.l.google.com:19302'},
+  {urls:'stun:stun1.l.google.com:19302'},
+  {urls:'stun:stun2.l.google.com:19302'},
+  {urls:'stun:stun3.l.google.com:19302'},
+  {urls:'stun:stun4.l.google.com:19302'},
+  /*{urls:'stun:stunserver.org'},
+  {urls:'stun:stun.softjoys.com'},
+  {urls:'stun:stun.voiparound.com'},
+  {urls:'stun:stun.voipbuster.com'},
+  {urls:'stun:stun.voipstunt.com'},
+  {urls:'stun:stun.voxgratia.org'},
+  {urls:'stun:stun.xten.com'},*/
+];
+
+
+// init peer connection
+let peerConnection = new RTCPeerConnection({
+  iceServers,
+});
+
+peerConnection.onicecandidate = function (e) {
+  if (e.candidate) {
+    console.log('onicecandidate')
+    channel.trigger(iceEventName, {
+      target: 'callee',
+      candidate: e.candidate,
+    })
+  }
+}
+
+channel.bind(iceEventName, (data) => {
+  console.log('ice received')
+  var candidate = new RTCIceCandidate(data.candidate);
+  peerConnection.addIceCandidate(candidate);
+});
+
+channel.bind(answerEventName, async (data) => {
+  console.log('on answer')
+  const { sdp } = data;
+  var desc = new RTCSessionDescription(sdp);
+  await peerConnection.setRemoteDescription(desc);
+});
+
+// utility
+function toRgbaArrayBuffer(buf) {
+  var ab = new ArrayBuffer(buf.length * 4 / 3);
+  var view = new Uint8Array(ab);
+  let c = 0;
+  for (var i = 0; i < buf.length; ++i) {
+    view[c] = buf[i];
+    c++;
+    if ((i + 1) % 3 === 0) {
+      view[c] = 255;
+      c++;
+    }
+  }
+  return ab;
+}
+// webrtc video (nonstandard)
+const source = new RTCVideoSource();
+const track = source.createTrack();
+const stream = new MediaStream();
+stream.addTrack(track);
+// camera
+function cameraProcedure(nh) {
+	const cameraTriggerInterval = 300;
+	let cameraTriggerAllowed = true;
+	/*setInterval(() => {
+		cameraTriggerAllowed = true;
+	}, cameraTriggerInterval);*/
+	nh.subscribe(
+		'/camera/rgb/image_raw',
+		'sensor_msgs/Image',
+		msg => {
+			//if (cameraTriggerAllowed) {
+        const { width, height } = msg;
+        const rgbaData = toRgbaArrayBuffer(msg.data);
+        const i420Data = new Uint8ClampedArray(width * height * 1.5);
+        const i420Frame = { width, height, data: i420Data };
+        const rgbaFrame = { width, height, data: rgbaData };
+        rgbaToI420(rgbaFrame, i420Frame)
+        source.onFrame(i420Frame);
+        cameraTriggerAllowed = false;
+			//}
+		},
+	);
+}
+
+async function start() {
+  await rosnodejs.initNode('/webrtc');
+  const nh = rosnodejs.nh;
+  cameraProcedure(nh);
+
+  await delay(1000);  // waiting for pusher to initialize
+  await peerConnection.addTrack(track, stream);
+  const offer = await peerConnection.createOffer();
+  await peerConnection.setLocalDescription(offer);
+  channel.trigger(offerEventName, {
+    name: 'caller',
+    target: 'callee',
+    sdp: peerConnection.localDescription,
+  });
+}
+
+start()
+  .catch(err => {
+    console.error(err);
+  });
